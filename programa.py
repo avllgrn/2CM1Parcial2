17c04e30a7aca2efddaa035fbca250d8d4fcfebb
Alfredo Villagran Olguin <avillagran@ipn.mx>
2025-04-29 08:18:29 -0600

    Funcion recursiva, multiplicacion

--- programa.py
+++ programa.py
@@ -1,12 +1,17 @@
 from os import system
 
-def funcionRecursiva(n):
-    print(f'funcionRecursiva con n={n}')
-    if n>1:
-        funcionRecursiva(n-1)
-    print(f'funcionRecursiva con n={n}')
+def multiplicacionRecursiva(a, b):
+    # print(f'multiplicacionRecursiva({a}, {b})')
+    if b>1:
+        return a + multiplicacionRecursiva(a, b-1)
+    else:
+        return a
 
 if __name__ == '__main__':
     system('cls')
 
-    funcionRecursiva(10)
+    a = int(input('Ingresa a '))
+    b = int(input('Ingresa b '))
+    c = multiplicacionRecursiva(a, b)
+
+    print(f'{a} * {b} = {c}')
